b7260ae01adc8af087348aab29535e915f140eae
MarieCamillePetit <MarieCamillePetit@outlook.com>
2023-03-27 14:20:48 +0200

    Modification cors pour vercel

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+
+//cors
 var cors = require("cors");
 
 var indexRouter = require("./routes/index");
@@ -26,6 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+//cors
 app.use(cors());
 
 app.use("/", indexRouter);
